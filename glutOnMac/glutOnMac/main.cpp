a87ce8ca52ed8168343bd50a2976ba5cd0020bee
Will Yang <wkyjyy@gmail.com>
2013-03-28 15:19:57 +0800

    playing with glut

--- glutOnMac/glutOnMac/main.cpp
+++ glutOnMac/glutOnMac/main.cpp
@@ -15,12 +15,19 @@ int main(int argc, char * argv[])
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
     glutInitWindowSize(250, 250);
-    glutInitWindowPosition(100, 100);
+    glutInitWindowPosition(250, 250);
     glutCreateWindow(argv[0]);
     init();
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
     glutMouseFunc(mouse);
+    std::cout << glGetString(GL_VERSION) << std::endl;
+    std::cout << glGetString(GL_VENDOR) << std::endl;
+    int i;
+    glGetIntegerv(GL_MAX_TEXTURE_UNITS, &i);
+    std::cout<<"max texture units:"<<i<<std::endl;
+    glGetIntegerv(GL_MAX_TEXTURE_SIZE, &i);
+    std::cout<<"max texture size:"<<i<<std::endl;
     glutMainLoop();
     return 0;
 }
--- glutOnMac/glutOnMac/render.cpp
+++ glutOnMac/glutOnMac/render.cpp
@@ -7,21 +7,84 @@
 //
 
 #include "render.h"
+#include <cmath>
+
+const static float PI = 3.14159265359;
+const static int circle_points = 50;
+
+const static GLint vertices[] = {
+    25, 25,
+    100, 325,
+    175, 25,
+    175, 325,
+    250, 25,
+    325, 325
+};
+
+const static GLint real_vertices[] = {
+    0, -25,
+    25, 50,
+    -25, 70
+};
+const static GLubyte indices[] = {0, 1, 2};
+
+const static GLfloat colors[] ={
+    1, 0.2, 0.2,
+    0.2, 0.2, 1,
+    0.8, 1, 0.2,
+    0.75, 0.75, 0.75,
+    0.35, 0.35, 0.35,
+    0.5, 0.5, 0.5
+};
 
 static GLfloat spin;
 
 void init(){
-    glClearColor(0.5, 0.3, 0.1, 0);
+    glClearColor(1, 1, 1, 1);
     glShadeModel(GL_FLAT);
 }
 
 void display(){
     glClear(GL_COLOR_BUFFER_BIT);
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_COLOR_ARRAY);
+    //glEnableClientState(GL_NORMAL_ARRAY);
+    glVertexPointer(2, GL_INT, 0, real_vertices);
+    glColorPointer(3, GL_FLOAT, 0, colors);
+    /*glBegin(GL_TRIANGLES);
+    {
+        glArrayElement(0);
+        glArrayElement(1);
+        glArrayElement(2);
+    }
+    glEnd();*/
+    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_BYTE, indices);
     glPushMatrix();
     glRotatef(spin, 0, 0, 1);
     glColor3f(1, 0.8, 0.3);
     glRectf(-25, -25, 25, 25);
     glPopMatrix();
+    glColor3f(1, 0.2, 0.1);
+    glLineStipple(1, 0xFFFF);
+    glLineWidth(5);
+    glBegin(GL_LINE_LOOP);
+    {
+        for (int i = 0; i < circle_points; i++) {
+            float angle = 2 * PI * i / circle_points;
+            glVertex2f(50 * cos(angle), 50 * sin(angle));
+        }
+    }
+    glEnd();
+    glColor3f(0.1, 0.8, 0.1);
+    glLineStipple(1, 0x0F0F);
+    glEnable(GL_LINE_STIPPLE);
+    glBegin(GL_LINES);
+    {
+        glVertex2f(-30, -30);
+        glVertex2f(30, 30);
+    }
+    glEnd();
+    
     glutSwapBuffers();
 }
 
@@ -44,8 +107,15 @@ void spinDisplay(){
 void mouse(int button, int state, int x, int y){
     switch (button) {
         case GLUT_LEFT_BUTTON:
-            if (state == GLUT_DOWN)
+            if (state == GLUT_DOWN){
                 glutIdleFunc(spinDisplay);
+//                glColor3f(0, 0, 0);
+//                glBegin(GL_POINTS);
+//                {
+//                    glVertex2f(x, y);
+//                }
+//                glEnd();
+            }
             break;
         case GLUT_RIGHT_BUTTON:
             if (state == GLUT_DOWN)
