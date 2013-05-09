bb2ff2517ae769d9766192b4196cba1a3de43259
chloechien <you@example.com>
2013-05-09 21:49:39 +0800

    profile.c

--- glutOnMac/profile.c
+++ glutOnMac/profile.c
@@ -1,23 +1,23 @@
 
-#include stdafx.h
-#include stdio.h
-#include GLglut.h
-#include glglext.h
-#include stdio.h
-#include iostream
-#includestring
-#includefstream
-#includesstream
+#include "stdafx.h"
+#include <stdio.h>
+#include <GL/glut.h>
+#include <gl/glext.h>
+#include <stdio.h>
+#include <iostream>
+#include<string>
+#include<fstream>
+#include<sstream>
 using namespace std;
 
 
 int v_num=34835; 
 int f_num=69666; 
 int vn_num=34835;
-GLfloat point; 
-int fpoint; 
-int fnArr;
-GLfloat vnArr;
+GLfloat **point; 
+int **fpoint; 
+int **fnArr;
+GLfloat **vnArr;
 string s1,s2,s3,s4;
 GLfloat f2,f3,f4;
 struct vectors
@@ -29,19 +29,19 @@ struct vectors
 int readfile(string addrstr) 
 {
 	int i;
-	point=new GLfloat[v_num];
-	for (i=0;iv_num;i++)
+	point=new GLfloat*[v_num];
+	for (i=0;i<v_num;i++)
 	{
 		point[i]=new GLfloat[3];
 	}
-	vnArr=new GLfloat[vn_num];
-	for (i=0;ivn_num;i++)
+	vnArr=new GLfloat*[vn_num];
+	for (i=0;i<vn_num;i++)
 	{
 		vnArr[i]=new GLfloat[3];
 	}
-	fpoint=new int[f_num];
-	fnArr=new int[f_num];
-	for (i=0;if_num;i++)
+	fpoint=new int*[f_num];
+	fnArr=new int*[f_num];
+	for (i=0;i<f_num;i++)
 	{
 		fpoint[i]=new int[3];
 		fnArr[i]=new int[3];
@@ -60,7 +60,7 @@ int readfile(string addrstr)
 			{
 				
 				istringstream sin(sline);
-				sins1f2f3f4;
+				sin>>s1>>f2>>f3>>f4;
 				vnArr[ii][0]=f2;
 				vnArr[ii][1]=f3;
 				vnArr[ii][2]=f4;
@@ -71,7 +71,7 @@ int readfile(string addrstr)
 			{
 				
 				istringstream sin(sline);
-				sins1f2f3f4;
+				sin>>s1>>f2>>f3>>f4;
 				point[jj][0]=f2;
 				point[jj][1]=f3;
 				point[jj][2]=f4;
@@ -85,25 +85,25 @@ int readfile(string addrstr)
 			istringstream in(sline);
 			
 			int a;
-			ins1;
+			in>>s1;
 			
 			int i,k;
 			
-			for(i=0;i3;i++)
+			for(i=0;i<3;i++)
 			{
-				ins1;
+				in>>s1;
 				
 				a=0;
-				for(k=0;s1[k]!='';k++)
+				for(k=0;s1[k]!='/';k++)
 				{
-					a=a10+(s1[k]-48);
+					a=a*10+(s1[k]-48);
 				}
 				fpoint[kk][i]=a;
 				
 				a=0;
-				for(k=k+2;ks1.size();k++)
+				for(k=k+2;k<s1.size();k++)
 				{
-					a=a10+(s1[k]-48);
+					a=a*10+(s1[k]-48);
 				}
 				fnArr[kk][i]=a;
 			}	
@@ -118,7 +118,7 @@ int readfile(string addrstr)
 
 void init(void)
 {
-	readfile(b.obj);
+	readfile("b.obj");
 	glClearColor(1.0,1.0,1.0,0.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
@@ -132,25 +132,25 @@ void display(void)
 	vectors R,N,E;
 	GLfloat m,u,v;
 	
-    for (int i=0;iv_num;i++)
+    for (int i=0;i<v_num;i++)
 	{
 		N.x=vnArr[i][0];
 		N.y=vnArr[i][1];
-		N.z=vnArr[i][2];顶点法向量
+		N.z=vnArr[i][2];//顶点法向量
 		E.x=point[i][0];
 		E.y=point[i][1];
 		E.z=point[i][2]-30;
-		float alfa=N.xE.x+N.yE.y+N.zE.z;
-		if(alfa20alfa5)continue;
-		R.x=(2alfa-1)E.x;
-		R.y=(2alfa-1)E.y;
-		R.z=(2alfa-1)E.z;
-		m=sqrt(R.xR.x+R.yR.y+(R.z+1)(R.z+1));
-		u=R.x(2m)+0.5;
-		v=R.y(2m)+0.5;
+		float alfa=N.x*E.x+N.y*E.y+N.z*E.z;
+		if(alfa>20||alfa<5)continue;
+		R.x=(2*alfa-1)*E.x;
+		R.y=(2*alfa-1)*E.y;
+		R.z=(2*alfa-1)*E.z;
+		m=sqrt(R.x*R.x+R.y*R.y+(R.z+1)*(R.z+1));
+		u=R.x/(2*m)+0.5;
+		v=R.y/(2*m)+0.5;
 		glPointSize(3.0f);  
 		glBegin(GL_POINTS);
-		glVertex2d(u200, v200);
+		glVertex2d(u*200, v*200);
 		glEnd();  
 	}
 
@@ -159,10 +159,10 @@ void display(void)
 	
 }
 
-int main(int argc, char argv)
+int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE  GLUT_RGB);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize(500, 500);
     glutInitWindowPosition(100, 100);
     glutCreateWindow(argv[0]);
