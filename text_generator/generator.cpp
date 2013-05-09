110e1a898d880242d932e3416ff9744ff390c0d3
wuhaotian <thelongestmailaccounteverexist@gmail.com>
2013-05-09 08:03:07 +0800

    a simple generator

--- text_generator/generator.cpp
+++ text_generator/generator.cpp
@@ -3,141 +3,107 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
-void ConvertCombine(IplImage * src,IplImage * dst)
-{
-	float theta = rand()%100;
-	theta -= 50;
-	theta /= 10;
+#include <iostream>
+#include <random>
 
-	
-	IplImage * middle = cvCloneImage(src) ;
-	middle->origin = src->origin;
-	cvZero(middle);//旋转的中间结果保存在middle
-	CvMat* rot_mat = cvCreateMat(2,3,CV_32FC1);//建立空矩阵作为旋转矩阵
-	CvPoint2D32f center = cvPoint2D32f(src->width/2,src->height/2);//图像中心为旋转中心
-	double scale = 20 +(rand()%160);
-	scale /= 100;//大小随机缩放80%
-	cv2DRotationMatrix(center,theta,scale,rot_mat);
-	//进行平移变换，平移变换的量是正负256随机
-	*( (float*)CV_MAT_ELEM_PTR(*rot_mat,0,2)) += rand()%512 - 256;
-	*( (float*)CV_MAT_ELEM_PTR(*rot_mat,1,2)) += rand()%512 - 256 + 20;
-	cvWarpAffine(src,middle,rot_mat);
-	
-	/*float mov[] = {	1,0,rand()%512 - 256,
-					0,1,rand()%512 - 256};
-	CvMat mov_mat = cvMat(2,3,CV_32FC1,mov);
-	
-	cvWarpAffine(middle,middle2,&mov_mat);*/
-	//进行alpha混合
-	IplImage * middle2 = cvCloneImage(dst) ;
-	cvAddWeighted(middle2,1.0,middle,(float)(rand()%50)/100 + 0.5,0,dst);
-	//cvNamedWindow("middle");
-	//cvShowImage("middle",dst);
-	//cvWaitKey(0);
-	cvReleaseImage(&middle);
-	cvReleaseImage(&middle2);
-	//cvDestroyWindow("middle");
+#pragma comment(lib,"highgui.lib")
+#pragma comment(lib,"cxcore.lib")
+#pragma comment(lib,"cv.lib")
 
-}
-int main()
-{
-	srand((unsigned)time(0));
-	cvNamedWindow("src");
-	cvNamedWindow("dst");
-	IplImage * src = cvLoadImage("stokes.bmp",0);
-	cvShowImage("src",src);
-	cvNot(src,src);
+#define margin 20
+#define variance 8.0
+#define range 11
+#define mean 0
 
-	IplImage * dst = cvCreateImage(cvGetSize(src),src->depth,src->nChannels);
-	cvCopy(src,dst);//创建目标图像
-	for(int now = 1; now <= 32 ; now ++)
+double cumulativeNormal(int x) {
+	static bool dispatch = false;
+	static double p[margin] = {};
+
+	if (!dispatch)
 	{
-		IplImage * dstcopy = cvCloneImage(dst);
-		for(int i = 0; i < 70 ; i++)
-		{
-			ConvertCombine(src,dst);//每一次随机生成一个线条，加入目标图像
-		}//进行评分
-		CvScalar score = cvAvg(dst);
-		printf("%02d:%d\n",now,(int)score.val[0]);
-		if(now <= 20)
-		{
-			if((int)score.val[0] != now * 7)
-			{
-				now --;
-				cvReleaseImage(&dst);
-				dst = cvCloneImage(dstcopy);
-			}
-			else
-			{
-				cvNot(dst,dst);
-				char filename[100];
-				sprintf(filename,"%02d.bmp",now);
-				cvSaveImage(filename,dst);
-				cvNot(dst,dst);
-			}
+		const int nrolls = 1000000;  // number of experiments
+
+		std::default_random_engine generator;
+		std::normal_distribution<double> distribution(0, variance);
+
+
+		for (int i = 0; i < nrolls; ++i) {
+			double number = distribution(generator);
+			if ((number >= 0.0 ) && (number <= margin)) ++p[int(number)];
 		}
-		else if(now <= 25)
-		{
-			if((int)score.val[0] != now * 7 - (now - 20))
-			{
-				now --;
-				cvReleaseImage(&dst);
-				dst = cvCloneImage(dstcopy);
-			}
-			else
-			{
-				cvNot(dst,dst);
-				char filename[100];
-				sprintf(filename,"%02d.bmp",now);
-				cvSaveImage(filename,dst);
-				cvNot(dst,dst);
-			}
+
+		for (int i = 0; i < margin; ++i) {
+			p[i] /= nrolls;
+			// p[i] /= p[0];
+			// p[i] = sqrt(p[i]);
+
+			printf("%f\n", p[i]);
 		}
-		else if(now < 30)
-		{
-			if((int)score.val[0] != 170 + (now-25)*5)
-			{
-				now --;
-				cvReleaseImage(&dst);
-				dst = cvCloneImage(dstcopy);
-			}
-			else
-			{
-				cvNot(dst,dst);
-				char filename[100];
-				sprintf(filename,"%02d.bmp",now);
-				cvSaveImage(filename,dst);
-				cvNot(dst,dst);
-			}
-		}
-		else
-		{
-			if((int)score.val[0] != 190 + (now-29)*4)
-			{
-				now --;
-				cvReleaseImage(&dst);
-				dst = cvCloneImage(dstcopy);
-			}
-			else
-			{
-				cvNot(dst,dst);
-				char filename[100];
-				sprintf(filename,"%02d.bmp",now);
-				cvSaveImage(filename,dst);
-				cvNot(dst,dst);
-			}
-		}
-		cvReleaseImage(&dstcopy);
-		//cvShowImage("dst",dst);
-		//cvWaitKey(100);
-		//cvNot(dst,dst);
+		dispatch = true;
 	}
-	cvNot(dst,dst);
-	cvShowImage("dst",dst);
-	cvWaitKey(0);
-	cvReleaseImage(&dst);
-	cvReleaseImage(&src);
-	cvDestroyWindow("src");
-	return(0);
+	return p[x % margin];
 }
+
+
+int main( int argc, char** argv ) 
+{
+	IplImage* image = cvCreateImage(cvSize(512,64),IPL_DEPTH_8U,3);
+	if(!image) 
+	{
+		return -1;
+	}
+	CvScalar start = CV_RGB(25, 25, 25);
+	CvScalar end = CV_RGB(255, 255, 255);
+	CvScalar bg = CV_RGB(255, 255, 255);
+
+	cvSet(image, bg);
+	double weightB = (end.val[0] - start.val[0]) / image->width;
+	double weightG = (end.val[1] - start.val[1]) / image->width;
+	double weightR = (end.val[2] - start.val[2]) / image->width;
+
+	for(int i = 0; i < image->width; i++)
+	{		
+		for(int j = margin;j < image->height - margin; j++)
+		{
+			double dist = image->width - i;
+			uchar* ptr = &CV_IMAGE_ELEM(image, uchar, j, i * 3);
+			ptr[0] = cvRound(ptr[0] + weightB*dist);
+			ptr[1] = cvRound(ptr[1] + weightG*dist);
+			ptr[2] = cvRound(ptr[2] + weightR*dist); 
+		}
+	}
+
+	// cvNot(image, image);
+
+	for (int i = 0; i < image->width; ++i)
+	{
+		uchar* origin = &CV_IMAGE_ELEM(image, uchar, margin, i * 3);
+		for (int j = 0; j < margin; ++j)
+		{
+			double factor = cumulativeNormal(margin - j - 1) / cumulativeNormal(0);
+			printf("%f\n", factor);
+			uchar* ptr = &CV_IMAGE_ELEM(image, uchar, j, i * 3);
+
+			ptr[0] = cvRound(255 - (255 - origin[0]) * factor);
+			ptr[1] = cvRound(255 - (255 - origin[1]) * factor);
+			ptr[2] = cvRound(255 - (255 - origin[2]) * factor);
+			// printf("%d %d\n", j, ptr[0]);
+			ptr = &CV_IMAGE_ELEM(image, uchar, image->height - 1 - j, i * 3);
+
+			ptr[0] = cvRound(255 - (255 - origin[0]) * factor);
+			ptr[1] = cvRound(255 - (255 - origin[1]) * factor);
+			ptr[2] = cvRound(255 - (255 - origin[2]) * factor);
+		}
+	}
+	// cvNot(image, image);
+
+	cvSaveImage( "radial.jpg", image );
+	cvNamedWindow( "test", 1 );
+	cvShowImage( "test", image );
+	cvWaitKey();
+	cvDestroyWindow("test");
+	cvReleaseImage(&image);
+	return 0; 
+}
+
 
