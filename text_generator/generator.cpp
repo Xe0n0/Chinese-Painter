6df339465f6f359d427a6d1fe9f13c7e76ad86c4
wuhaotian <thelongestmailaccounteverexist@gmail.com>
2013-05-11 12:39:46 +0800

    ad Markcov

--- text_generator/generator.cpp
+++ text_generator/generator.cpp
@@ -13,6 +13,73 @@
 #define margin 20
 #define mean 0
 
+int markov(int now, int max, int p_return, int p_inc, int p_dec)
+{
+	int seed = random() % 10;
+	if (now == max || now == -max)
+	{
+		if (seed < p_return)
+		{
+			return -1;
+		}
+	}
+	else {
+		if (seed < p_inc)
+		{
+			return 1;
+		}
+		else if (seed < p_dec + p_inc)
+		{
+			return -1;
+		}
+	}
+	return 0;
+}
+
+void randomSpot(IplImage* image, int num_spot, int len)
+{
+	std::default_random_engine generator;
+	std::normal_distribution<double> distribution(len, 5);
+	while (num_spot--)
+	{
+		int w_start = random() % (image->width - 2 * margin) + margin;
+		int h_start = random() % (image->height - 2 * margin) + margin;
+		int v = random() % 5;	
+
+		double number = distribution(generator);
+
+		int size = random() % 5 + 1;
+		int range = 2 * size + 1;
+
+		for (int i = 0; i < int(number); ++i)
+		{
+			w_start += size;
+			// h_start += markov(h_start, 1, 9, 1, 1);
+
+			for (int j = 0; j < range; ++j)
+			{
+				
+				for (int k = 0; k < range; ++k)
+				{
+					int h = h_start - size + j;
+					int w = w_start - size + k;
+					if (h >= image->height || h < 0 || w >= image->width || w < 0)
+					{
+						continue;
+					}
+					uchar* ptr = &CV_IMAGE_ELEM(image, uchar, h_start - 1 + j, (w_start - 1 + k) * 3);
+
+					ptr[0] = cvRound(fmin(v + ptr[0], 255));
+					ptr[1] = cvRound(fmin(v + ptr[1], 255));
+					ptr[2] = cvRound(fmin(v + ptr[2], 255));
+
+				}
+			}
+			
+		}
+	}
+}
+
 static double distribution[20][margin];
 
 void initial()
@@ -63,7 +130,7 @@ double cumulativeNormal(int x, int variance) {
 	return p[(x % margin)];
 }
 
-IplImage* calc_with_variance(int variance)
+IplImage* calc_with_variance()
 {
 	IplImage* image = cvCreateImage(cvSize(512,64),IPL_DEPTH_8U,3);
 	CvScalar start = CV_RGB(25, 25, 25);
@@ -86,9 +153,12 @@ IplImage* calc_with_variance(int variance)
 			ptr[2] = cvRound(ptr[2] + weightR*dist); 
 		}
 	}
-	printf("main stroke\n");
 	// cvNot(image, image);
+	return image;
+}
 
+void halo(IplImage *image, int variance)
+{
 	for (int i = 0; i < image->width; ++i)
 	{
 		uchar* origin = &CV_IMAGE_ELEM(image, uchar, margin, i * 3);
@@ -109,31 +179,39 @@ IplImage* calc_with_variance(int variance)
 			ptr[2] = cvRound(255 - (255 - origin[2]) * factor);
 		}
 	}
-	
-	return image;
 }
 
-int main( int argc, char** argv ) 
+int main(int argc, char** argv ) 
 {
 	initial();
-	for (int j = 1; j < 20; j += 2)
+	for (int j = 3; j < 5; j += 2)
 	{
 		for (int i = 5; i < 15; ++i)
 		{
-			IplImage *image = calc_with_variance(i);
-			char filename[100];
+			for (int k = 4; k < 10; k += 2)
+			{
+				for (int l = 300; l < 500; l += 20)
+				{
+					IplImage *image = calc_with_variance();
+					char filename[100];
+					
+					IplImage* image_out = cvCreateImage(cvSize(512,64),IPL_DEPTH_8U,3);
+					randomSpot(image, k, l);
+
+					halo(image, i);
+
+					cvSmooth(image, image_out, CV_GAUSSIAN, 3, 3, j + 10);
+
+					sprintf(filename, "./texture/ND_GB_RS_Marcov/texture_b%d_d%d_N%d_L%d.jpg", j, i, k, l);
+					printf("generating %s\n", filename);
+					cvSaveImage(filename, image_out);
+					cvNamedWindow( "test", CV_WINDOW_AUTOSIZE);
+					cvShowImage( "test", image_out);
+
+					cvReleaseImage(&image);
+				}
+			}
 			
-			IplImage* image_out = cvCreateImage(cvSize(512,64),IPL_DEPTH_8U,3);
-
-			cvSmooth(image, image_out, CV_GAUSSIAN, 3, 3, j);
-
-			sprintf(filename, "./texture/ND_GB/texture_b%d_d%d.jpg", j, i);
-			printf("generating %s\n", filename);
-			cvSaveImage(filename, image_out);
-			cvNamedWindow( "test", CV_WINDOW_AUTOSIZE);
-			cvShowImage( "test", image_out);
-
-			cvReleaseImage(&image);
 		}
 	}
 	cvWaitKey();
